dabd779887165926777561c220e3011bf161d028
patrickyvens <yvens.dosmeuspro@gmail.com>
2024-09-17 14:26:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,23 +7,28 @@ int main()
 {
     int choix;
     int somme;
-    MonCompte Dupont;
- 
-    cout << "1. Deposer de l'argent" << endl << "2. Retirer de l'argent" << endl;
+    MonCompte Dosmeus;
+
+
+
+
+    cout << "Bonjour veuiller Choisir S'il vous plait" << endl;
+
+    cout << "1. Deposer de l'argent" << endl << "2. Retirer de l'argent" << endl << "3. consulter"    << endl;
     cin >> choix;
  
     if (choix == 1)
     {
         cout << "Combien voulez-vous deposer ? ";
         cin >> somme;
-        Dupont.deposerArgent(somme);
+        Dosmeus.deposerArgent(somme);
     }
  
     else if (choix == 2)
     {
         cout << "Combien voulez vous retirer ? ";
         cin >> somme;
-        Dupont.retirerArgent(somme);
+        Dosmeus.retirerArgent(somme);
     }
  
  
