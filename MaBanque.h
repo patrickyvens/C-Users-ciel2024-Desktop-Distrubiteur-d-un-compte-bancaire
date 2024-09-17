4a59baedf87f366df477709d9ad19198f50bc906
patrickyvens <yvens.dosmeuspro@gmail.com>
2024-09-17 13:04:46 +0200

    Update MaBanque.h

--- MaBanque.h
+++ MaBanque.h
@@ -1,1 +1,24 @@
+#ifndef DEF_MABANQUE
+#define DEF_MABANQUE
+
+class MonCompte
+{
+    public:
+        MonCompte();
+        ~MonCompte();
+        void deposerArgent(int somme);
+        void retirerArgent(int somme);
+        void consulter();
+
+
+    private:
+
+        int m_solde;
+        std::string m_nom;
+           
+
+
+};
+
+#endif
 
