5c3f78bef7598a94a4027a6ed43a224d02bf865b
patrickyvens <yvens.dosmeuspro@gmail.com>
2024-09-17 13:07:21 +0200

    Create MaBanque.cpp

---
+++ MaBanque.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "MaBanque.h"
+
+using namespace std;
+
+MonCompte::MonCompte()
+{
+    m_solde = 200;
+    m_nom = "DOSMEUS";
+}
+
+
+//inutile mais je préfère prendre l'habitude
+MonCompte::~MonCompte()
+{
+}
+
+void MonCompte::deposerArgent(int somme)
+{
+    m_solde = m_solde + somme;
+    cout << "Le montant que vous avez deposer mr est :" << somme <<"euros. Et votre solde est actuelment de " << m_solde << "euros." << endl;
+
+}
+
+void MonCompte::retirerArgent(int somme)
+{
+    if (somme > m_solde)
+        cout << "Vous n'avez pas essez argent." << endl;
+
+    else
+    {
+        m_solde = m_solde - somme;
+        cout << "Actuelmant votre solde est de " << m_solde <<" euros." << endl;
+
+    }    
+
+}
