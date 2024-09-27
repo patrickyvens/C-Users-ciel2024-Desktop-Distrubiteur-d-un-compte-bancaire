8357b2b293ee0dca41f4ba990895ee3ae327b56f
patrickyvens <yvens.dosmeuspro@gmail.com>
2024-09-27 14:06:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,91 +1,114 @@
 #include <iostream>
-#include <string>
-#include "MaBanque.h"
-
+#include "Client.h"
 
 using namespace std;
- 
 
-
-
-int main()
-{
-
- cout << "Programme de gestion de comptes bancaires." << endl;
-  cout << "Auteur : Yvens Dosmeus" << endl;
-  cout << "Cadre  : TP Informatique" << endl;
-  cout << "Date   : 17/09/2024" << endl;
-
-    int choix;
-    int somme;
-    MonCompte Dosmeus;
-    
-
+int main() {
+    cout << "Programme de gestion de comptes bancaires." << endl;
+    cout << "Auteur : Yvens Dosmeus" << endl;
+    cout << "Cadre  : TP C++ Informatique" << endl;
+    cout << "Date   : 17/09/2024" << endl;
 
     cout << "" << endl;
 
     cout << "-------------------------------------------" << endl;
     cout << "" << endl;
 
-    cout << " *** BONJOUR VEUILLER CHOISIR ***" << endl;
-   
-    cout << "1. Deposer de l'argent" << endl << "2. Retirer de l'argent" << endl << "3. consulter" << endl << "4. RIB" << endl << "5. Annuler le retrait" << endl;
-    cin >> choix;
- 
-    if (choix == 1)
-    {
-        cout << "Combien voulez-vous deposer ? ";
-        cin >> somme;
-        Dosmeus.deposerArgent(somme);
-    }
- 
-      else if (choix == 2)
-    {
-        cout << "Combien voulez vous retirer ? ";
-        cin >> somme;
-        Dosmeus.retirerArgent(somme);
+
+    Client cl1("Dosmeus Yvens", "ABC123456", "0605885332");
+
+    cout << "Bienvenue au distributeur automatique." << endl;
+    cout << "Veuillez choisir un compte (1 ou 2) : ";
+    int compteChoisi;
+    cin >> compteChoisi;
+
+    cout << "Veuillez entrer votre code de securite : ";
+    string codeSecurite;
+    cin >> codeSecurite;
+
+    if (!cl1.verifierCodeSecurite(compteChoisi, codeSecurite)) {
+        cout << "Code de securite incorrect." << endl;
+        return 1;
     }
 
-
-       else if  (choix == 3)
-    {
-        cout << "Consulter votre compte ? ";
-        cout << "Tapez 0  ";
-        cin >> somme;
-        Dosmeus.deposerArgent(somme);
+    Compte* compte;
+    if (compteChoisi == 1) {
+        compte = &cl1.getCompte1();
+    } else {
+        compte = &cl1.getCompte2();
     }
 
+    int choix;
+    do {
+        cout << "\n1. Deposer de l'argent" << endl;
+        cout << "2. Retirer de l'argent" << endl;
+        cout << "3. Consulter le solde" << endl;
+        cout << "4. Consulter RIB" << endl;
+        cout << "5. Transferer de l'argent" << endl;
+        cout << "6. Quitter" << endl;
+        cout << "Choisissez une option : ";
+        cin >> choix;
 
-       else if (choix == 4)
-    {
-        cout << "   RELEVER D'IDENTITE BANCAIRE " << endl;
+        switch (choix) {
+            case 1: {
+                double montant;
+                cout << "Montant a deposer : ";
+                cin >> montant;
+                compte->deposer(montant);
+                break;
+            }
+            case 2: {
+                double montant;
+                cout << "Montant a retirer : ";
+                cin >> montant;
+                if (!compte->retirer(montant)) {
+                    cout << "Solde insuffisant." << endl;
+                }
+                break;
+            }
+            case 3:
+                compte->afficherSolde();
+                break;
+            case 4:
+                compte->afficherRIB();
+                break;
+            case 5: {
+                cout << "Choisissez le compte destinataire (1 ou 2) : ";
+                int destinataire;
+                cin >> destinataire;
 
-        cout << "----------------------------------- " << endl;
+                // ici il demande le code du compte dest 
+                cout << "Veuillez entrer le code de securite du compte destinataire : ";
+                string codeDestinataire;
+                cin >> codeDestinataire;
 
-        cout << "Nom du titulaire du compte   :    DOSMEUS YVENS " << endl;
-        cout << "le nom de la banque          :    Nickel   "      << endl;
-        cout << "le code banque               :    156487 "        << endl;
-        cout << "le numéro du compte         :    37905867842"    << endl;
-        cout << "la clé RIB du code          :    12"             << endl;
-        cout << "l’IBAN du compte           :    ***** "         << endl;
-        cout << "le code BIC.                 :    ***** "         << endl;
+                // et ici il verifier 
+                if (!cl1.verifierCodeSecurite(destinataire, codeDestinataire)) {
+                    cout << "Code de securite du compte destinataire incorrect." << endl;
+                    break;
+                }
 
-        cout << "Tapez 0  ";
-        
-        
-    }
+                double montant;
+                cout << "Montant a transferer : ";
+                cin >> montant;
 
-       else if (choix == 5 )
-    {
-        cout << "Voulez vous vraiment annuler ? ";
-        cout << "Tapez 5 Pour confimer  ";
-        cin >> somme;
-        cout << "Annulation Reussi ! AU REVOIR ";
+                if (destinataire == 1) {
+                    cout <<" DOSMEUS YVENS" << endl;
+                    compte->transfererVers(cl1.getCompte1(), montant);
+                } else if (destinataire == 2) {
+                    compte->transfererVers(cl1.getCompte2(), montant);
+                } else {
+                    cout << "Compte destinataire invalide." << endl;
+                }
+                break;
+            }
+            case 6:
+                cout << "Au revoir !" << endl;
+                break;
+            default:
+                cout << "Option invalide." << endl;
+        }
+    } while (choix != 6);
 
-        
     return 0;
-    }
-
-
 }
-
